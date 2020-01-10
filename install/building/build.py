5551001d22014fd828754db8ba30570db9679c11
SwZ <e824750130@qq.com>
2020-01-10 16:28:17 +0800

    install module

--- install/building/build.py
+++ install/building/build.py
@@ -1,8 +1,10 @@
 from .database import build_db
+import os
 import subprocess
 
 
 def start():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoDT.settings')
     command = 'pip3 install -r ../../requirement/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/'
     p = subprocess.Popen(command, stdout=subprocess.PIPE)
     for line in iter(p.stdout.readline, b''):
@@ -11,3 +13,7 @@ def start():
     p.wait()
     print('构建数据库中..')
     build_db()
+
+
+if __name__ == '__main__':
+    start()
