769f14a461099ed8840a5813ae9b59ae2b21bd91
swz <e824750130@qq.com>
2020-04-22 20:22:55 +0800

    set db default charset

--- djangoDT/settings.py
+++ djangoDT/settings.py
@@ -75,6 +75,7 @@ DATABASES = {
         'HOST': server_conf.MYSQL_HOST,
         'PORT': 3306,
         'OPTIONS': {
+            'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
